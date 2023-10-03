6805a043e938eeff531d28ae580b40b9d69427cf
blubb12blubb3 <larsknittler@gmail.com>
2023-10-03 13:12:22 +0200

    Crash Fix: If language.value doesn't exist.

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -26,9 +26,11 @@ class Wiki(commands.Cog):
 
         search_text = "There is currently no text in this page. You can search for this page title in other pages, or search the related logs, but you do not have permission to create this page."
 
-    #Check for Page Anchor
+
+        #Check for Page Anchor
         if page_anchor == None:
             url = f"https://seaofthieves.fandom.com/wiki/{search}"
+            unformatted_page_anchor = None
             combined_search = search
         elif page_anchor != None:
         #page anchor formatting
@@ -38,13 +40,13 @@ class Wiki(commands.Cog):
 
             url = f"https://seaofthieves.fandom.com/wiki/{search}#{page_anchor}"
             combined_search = f"{search}#{page_anchor}"
-    
-    #Check for Language
-        if page_anchor == "en":
-            page_anchor = None
-        elif language.value != None:
-            url = f"https://seaofthieves.fandom.com/{language.value}/wiki/{combined_search}"
-    #Check if Article exists
+        #Check for Language
+        if language == None:
+            url_lang = "en"
+        else:
+            url_lang = language.value
+            url = f"https://seaofthieves.fandom.com/{url_lang}/wiki/{combined_search}"
+        #Check if Article exists
         response = requests.get(url)
         if response.status_code == 200:
             page_content = response.text
@@ -52,12 +54,12 @@ class Wiki(commands.Cog):
                 await interaction.response.send_message(f"Geb bitte blubb Bescheid, wenn ich das hier ausgespuckt habe. Dann ist etwas mit mir falsch (Code: wiki03)") #ERROR CODE: wiki03
             else:
                 await interaction.response.send_message(url)
-    #That Article Doesn't Exist + Translations
+        #That Article Doesn't Exist + Translations
         else:
-        #Chinese (Simplified)
-            if language.value == "zh":
+            #Chinese (Simplified)
+            if url_lang == "zh":
                 embed=discord.Embed(title="这篇文章并不存在。", 
-                                    description=f"没有关于 **{search}** 的维基文章。确保拼写正确。您不必注意大小写，但一定不要忘记任何空格。", 
+                                    description=f'没有关于 **{search}** 的维基文章。确保拼写正确。您不必注意大小写，但一定不要忘记任何空格。', 
                                     color=0xc69c6d)
                 embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
                 if page_anchor != None:
@@ -65,8 +67,8 @@ class Wiki(commands.Cog):
                 elif page_anchor == None:
                     embed.add_field(name="page_anchor", value="-", inline=True)
                 embed.add_field(name="Link to all articles", value="https://seaofthieves.fandom.com/zh/wiki/Special:%E6%89%80%E6%9C%89%E9%A1%B5%E9%9D%A2", inline=False)
-        #Czech
-            elif language.value == "cs":
+            #Czech
+            if url_lang == "cs":
                 embed=discord.Embed(title="Tento článek neexistuje..", 
                                     description=f"Na wiki není **žádný** článek o **{search}**. Ujistěte se, že jste vše napsali správně. Nemusíte dbát na velká a malá písmena, ale ujistěte se, že jste nezapomněli na mezery.", 
                                     color=0xc69c6d)
@@ -77,7 +79,7 @@ class Wiki(commands.Cog):
                     embed.add_field(name="page_anchor", value="-", inline=True)
                 embed.add_field(name="Odkaz na všechny články", value="https://seaofthieves.fandom.com/cs/wiki/Speci%C3%A1ln%C3%AD:V%C5%A1echny_str%C3%A1nky", inline=False)
         #French
-            elif language.value == "fr":
+            elif url_lang == "fr":
                 embed=discord.Embed(title="Cet article n'existe pas.", 
                                     description=f"Il n'y a **aucun** article wiki sur **{search}**. Assurez-vous que vous avez tout orthographié correctement. Vous n'avez pas besoin de faire attention aux majuscules et aux minuscules, mais assurez-vous de ne pas oublier d'espaces.", 
                                     color=0xc69c6d)
@@ -88,7 +90,7 @@ class Wiki(commands.Cog):
                     embed.add_field(name="page_anchor", value="-", inline=True)
                 embed.add_field(name="Lien vers tous les articles", value="https://seaofthieves.fandom.com/fr/wiki/Sp%C3%A9cial:Toutes_les_pages", inline=False)
         #German"
-            elif language.value == "de":
+            elif url_lang == "de":
                 embed=discord.Embed(title="Diesen Artikel gibt es nicht.", 
                                     description=f"Es gibt **keinen** Wiki-Artikel über **{search}**. Vergewisse dich bitte, dass du alles richtig geschrieben hast. Du musst nicht auf Groß- und Kleinschreibung achten, aber stelle bitte sicher, dass du keine Leerzeichen vergessen hast.", 
                                     color=0xc69c6d)
@@ -99,7 +101,7 @@ class Wiki(commands.Cog):
                     embed.add_field(name="page_anchor", value="-", inline=True)
                 embed.add_field(name="Link zu allen Artikeln", value="https://seaofthieves.fandom.com/de/wiki/Spezial:Alle_Seiten", inline=False)
         #Portuguese
-            elif language.value == "pt":
+            elif url_lang == "pt":
                 embed=discord.Embed(title="Esse artigo não existe.", 
                                     description=f"Não existe **nenhum** artigo wiki sobre **{search}**. Certifica-te de que escreveste tudo corretamente. Não tens de prestar atenção às maiúsculas e minúsculas, mas certifica-te de que não te esqueces dos espaços..", 
                                     color=0xc69c6d)
@@ -110,7 +112,7 @@ class Wiki(commands.Cog):
                     embed.add_field(name="page_anchor", value="-", inline=True)
                 embed.add_field(name="Ligação a todos os artigos", value="https://seaofthieves.fandom.com/pt/wiki/Especial:Todas_as_p%C3%A1ginas", inline=False)
         #Spanish
-            elif language.value == "es":
+            elif url_lang == "es":
                 embed=discord.Embed(title="Ese artículo no existe.", 
                                     description=f"No hay **ningún** artículo wiki sobre **{search}**. Asegúrate de haber escrito todo correctamente. No tienes que prestar atención a las mayúsculas y minúsculas, pero asegúrate de no olvidar ningún espacio.", 
                                     color=0xc69c6d)
@@ -132,7 +134,7 @@ class Wiki(commands.Cog):
                     embed.add_field(name="page_anchor", value="-", inline=True)
                 embed.add_field(name="Link to all articles", value="https://seaofthieves.fandom.com/wiki/Special:AllPages", inline=False)
             embed.set_thumbnail(url=self.bot.user.avatar.url)
-            await interaction.response.send_message(embed=embed)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Wiki(bot))
