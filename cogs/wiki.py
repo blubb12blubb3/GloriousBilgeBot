b7af8809ff429145159119348495664d064c80ce
blubb12blubb3 <larsknittler@gmail.com>
2023-10-11 20:17:56 +0200

    huge wiki improvements

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -1,140 +1,136 @@
 from discord.ext import commands
 import discord
 from discord import app_commands
-import requests
+import asyncio
+import fandom
 
 class Wiki(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @app_commands.command(name="wiki", description="Durchsucht das SoT Wiki")
-#Language Choice
-    @app_commands.choices(language=[
-        app_commands.Choice(name="Chinese (Simplified)", value="zh"),
-        app_commands.Choice(name="Czech", value="cs"),
-        app_commands.Choice(name="English", value="en"),
-        app_commands.Choice(name="French", value="fr"),
-        app_commands.Choice(name="German", value="de"),
-        app_commands.Choice(name="Portuguese", value="pt"),
-        app_commands.Choice(name="Spanish", value="es"),])
-    
-    async def wiki(self, interaction: discord.Interaction, search: str, page_anchor: str = None, language: app_commands.Choice[str] = None):
-    #search formatting
-        unformatted_search = search
-        search = search.title()
-        search = search.replace(" ", "_")
+    @app_commands.command(name="wiki", description="Search a Fandom Wiki")
+    async def wiki(self, interaction: discord.Interaction, search: str, amount_of_results: int = None):
+        #Config
+        wikiname = "Sea of Thieves"
 
-        search_text = "There is currently no text in this page. You can search for this page title in other pages, or search the related logs, but you do not have permission to create this page."
-
-
-        #Check for Page Anchor
-        if page_anchor == None:
-            url = f"https://seaofthieves.fandom.com/wiki/{search}"
-            unformatted_page_anchor = None
-            combined_search = search
-        elif page_anchor != None:
-        #page anchor formatting
-            unformatted_page_anchor = page_anchor
-            page_anchor = page_anchor.title()
-            page_anchor = page_anchor.replace(" ", "_")
-
-            url = f"https://seaofthieves.fandom.com/wiki/{search}#{page_anchor}"
-            combined_search = f"{search}#{page_anchor}"
-        #Check for Language
-        if language == None:
-            url_lang = "en"
+        #set wiki
+        fandomname = wikiname.lower().replace(" ","")
+        fandom.set_wiki(fandomname)
+        
+        #amount of results reduced to 1 - 10
+        if amount_of_results == None:
+            results = 3
+        elif amount_of_results >= 9:
+            results = 9
+        elif amount_of_results <= 1:
+            results = 1
         else:
-            url_lang = language.value
-            url = f"https://seaofthieves.fandom.com/{url_lang}/wiki/{combined_search}"
-        #Check if Article exists
-        response = requests.get(url)
-        if response.status_code == 200:
-            page_content = response.text
-            if search_text in page_content:
-                await interaction.response.send_message(f"Geb bitte blubb Bescheid, wenn ich das hier ausgespuckt habe. Dann ist etwas mit mir falsch (Code: wiki03)") #ERROR CODE: wiki03
-            else:
-                await interaction.response.send_message(url)
-        #That Article Doesn't Exist + Translations
+            results = amount_of_results
+        
+        try:
+            #fandom search
+            result_list = fandom.search(search, results = results)
+            #names -> page titles (List)
+            names = []
+            #numbers -> page ids (List)
+            numbers = []
+            for i, (name, number) in enumerate(result_list, 1):
+                names.append(name)
+                numbers.append(number)
+        except Exception as ee:
+            print(ee)
+        if not result_list:
+            failEmbed=discord.Embed(title="No search results.", 
+                                    description=f"There are **no** search results for **{search}** in the {wikiname} Wiki.", 
+                                    color=0x2b2d31)
+            failEmbed.add_field(name="Link to all articles", value=f"https://{fandomname}.fandom.com/wiki/Special:AllPages", inline=False)
+            failEmbed.set_thumbnail(url=self.bot.user.avatar.url)
+            await interaction.response.send_message(embed=failEmbed, ephemeral=True)
+
+
+            await interaction.response.send_message(content=f'No search results were found for "**{search}**". Please try another search.')
+
+        #search result formatting
+        emoji_mapping = {
+        1: '1️⃣',
+        2: '2️⃣',
+        3: '3️⃣',
+        4: '4️⃣',
+        5: '5️⃣',
+        6: '6️⃣',
+        7: '7️⃣',
+        8: '8️⃣',
+        9: '9️⃣',
+        }
+        search_results = "\n".join([f"{emoji_mapping.get(i + 1, '❌')} {name}\n" for i, name in enumerate(names)])
+        #search results embed send
+        resultsEmbed=discord.Embed(title=f'Search Results for "{search}"', 
+                            description=f"{search_results}", 
+                            color=0x2b2d31)
+        await interaction.response.send_message(embed=resultsEmbed)
+
+        #add reactions to search result embed
+        msg = await interaction.original_response()
+        if 1 <= results <= 10:
+            for i in range(1, results + 1):
+                number_emoji = f"{i}\uFE0F\u20E3"
+                await msg.add_reaction(number_emoji)
         else:
-            #Chinese (Simplified)
-            if url_lang == "zh":
-                embed=discord.Embed(title="这篇文章并不存在。", 
-                                    description=f'没有关于 **{search}** 的维基文章。确保拼写正确。您不必注意大小写，但一定不要忘记任何空格。', 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Link to all articles", value="https://seaofthieves.fandom.com/zh/wiki/Special:%E6%89%80%E6%9C%89%E9%A1%B5%E9%9D%A2", inline=False)
-            #Czech
-            if url_lang == "cs":
-                embed=discord.Embed(title="Tento článek neexistuje..", 
-                                    description=f"Na wiki není **žádný** článek o **{search}**. Ujistěte se, že jste vše napsali správně. Nemusíte dbát na velká a malá písmena, ale ujistěte se, že jste nezapomněli na mezery.", 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Odkaz na všechny články", value="https://seaofthieves.fandom.com/cs/wiki/Speci%C3%A1ln%C3%AD:V%C5%A1echny_str%C3%A1nky", inline=False)
-        #French
-            elif url_lang == "fr":
-                embed=discord.Embed(title="Cet article n'existe pas.", 
-                                    description=f"Il n'y a **aucun** article wiki sur **{search}**. Assurez-vous que vous avez tout orthographié correctement. Vous n'avez pas besoin de faire attention aux majuscules et aux minuscules, mais assurez-vous de ne pas oublier d'espaces.", 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Lien vers tous les articles", value="https://seaofthieves.fandom.com/fr/wiki/Sp%C3%A9cial:Toutes_les_pages", inline=False)
-        #German"
-            elif url_lang == "de":
-                embed=discord.Embed(title="Diesen Artikel gibt es nicht.", 
-                                    description=f"Es gibt **keinen** Wiki-Artikel über **{search}**. Vergewisse dich bitte, dass du alles richtig geschrieben hast. Du musst nicht auf Groß- und Kleinschreibung achten, aber stelle bitte sicher, dass du keine Leerzeichen vergessen hast.", 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Link zu allen Artikeln", value="https://seaofthieves.fandom.com/de/wiki/Spezial:Alle_Seiten", inline=False)
-        #Portuguese
-            elif url_lang == "pt":
-                embed=discord.Embed(title="Esse artigo não existe.", 
-                                    description=f"Não existe **nenhum** artigo wiki sobre **{search}**. Certifica-te de que escreveste tudo corretamente. Não tens de prestar atenção às maiúsculas e minúsculas, mas certifica-te de que não te esqueces dos espaços..", 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Ligação a todos os artigos", value="https://seaofthieves.fandom.com/pt/wiki/Especial:Todas_as_p%C3%A1ginas", inline=False)
-        #Spanish
-            elif url_lang == "es":
-                embed=discord.Embed(title="Ese artículo no existe.", 
-                                    description=f"No hay **ningún** artículo wiki sobre **{search}**. Asegúrate de haber escrito todo correctamente. No tienes que prestar atención a las mayúsculas y minúsculas, pero asegúrate de no olvidar ningún espacio.", 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Enlace a todos los artículos", value="https://seaofthieves.fandom.com/es/wiki/Especial:Todas", inline=False)
-        #English (Default)
-            else:
-                embed=discord.Embed(title="That Article Doesn't Exist.", 
-                                    description=f"There is **no** wiki article on **{search}**. Make sure you have spelled everything correctly. You don't have to pay attention to upper and lower case but make sure to not forget any spaces.", 
-                                    color=0xc69c6d)
-                embed.add_field(name="search", value=f"{unformatted_search}", inline=True)
-                if page_anchor != None:
-                    embed.add_field(name="page_anchor", value=f"{unformatted_page_anchor}", inline=True)
-                elif page_anchor == None:
-                    embed.add_field(name="page_anchor", value="-", inline=True)
-                embed.add_field(name="Link to all articles", value="https://seaofthieves.fandom.com/wiki/Special:AllPages", inline=False)
-            embed.set_thumbnail(url=self.bot.user.avatar.url)
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await msg.add_reaction('❌')
+            
+        #check reactions
+        def check(reaction, user):
+            return user == interaction.user and reaction.message.id == msg.id     
+        try:
+            reaction = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+            emoji = reaction[0].emoji
+            user_reaction = None
+            for key, value in emoji_mapping.items():
+                if emoji == value:
+                    user_reaction = key
+                    break
+        except asyncio.TimeoutError:
+            await interaction.followup.send(content=f"{interaction.user.mention} did not react in time.")
+        #except Exception as e1:
+            #print(e1)
+        await msg.clear_reactions()
+
+        #loading page information
+        loadingIcon = "<a:loading:1161668117861249134> "
+        loadingEmbed=discord.Embed(title="",
+                            description=f"Loading page information {loadingIcon}",
+                            color=0x2b2d31)
+        await interaction.edit_original_response(embed=loadingEmbed)
+        
+        #send result
+        try:
+            page = fandom.page(pageid = numbers[user_reaction - 1])
+
+            page_title = page.title.replace(' ', '_')
+            base_url = f"https://{fandomname}.fandom.com/wiki/{page_title}#"
+            
+            input_list = page.sections
+            hyper_list = [f"[{item}]({base_url}{item.replace(' ', '_')})" for item in input_list]
+            hyperlinks = str(hyper_list).replace("'","")
+            hyperlinks = hyperlinks[1:-1]
+
+            contentEmbed=discord.Embed(title=f"{page.title}", 
+                            url=f"{page.url}",
+                            description=f"{page.summary}", 
+                            color=0x2b2d31)
+            contentEmbed.set_footer(text=f"{wikiname} Wiki")
+            contentEmbed.set_image(url=f"{page.images[0]}")
+            theLine = "⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯"
+            contentEmbed.add_field(name=theLine, value="**Sections: **", inline=False)
+            for link in hyper_list:
+                contentEmbed.add_field(name="", value=link, inline=False)
+            contentEmbed.add_field(name=theLine, value="", inline=False)
+            
+            await interaction.edit_original_response(embed=contentEmbed)
+        except Exception as e:
+            await interaction.delete_original_response()
+            await interaction.followup.send(content=f"The Fandom API could not retrieve information from the site.\n Here is the link: \n {base_url[:-1]}")
+            #print(e)
 
 async def setup(bot):
     await bot.add_cog(Wiki(bot))
