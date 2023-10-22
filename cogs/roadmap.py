07a836400c9a647000551180a35aca7db908fc4c
blubb12blubb3 <larsknittler@gmail.com>
2023-10-22 21:40:14 +0200

    moved roadmap to .txt file for ease of use

--- cogs/roadmap.py
+++ cogs/roadmap.py
@@ -9,15 +9,13 @@ class Roadmap(commands.Cog):
 
     @app_commands.command(name="roadmap", description="Was für die Zukunft geplant ist")
     async def roadmap(self, interaction: discord.Interaction):
-        embed=discord.Embed(title="Roadmap für den Glorious Bilge Bot", description="Hier steht was für die Zukunft von dem Bot geplant ist. Bei Wünschen, schreibt gerne eine Nachricht an blubb12blubb3.", color=0xc59d6d)
-        embed.set_thumbnail(url=self.bot.user.avatar.url)
-        #Ziel 1
-        embed.add_field(name="/help", value="- Ein Command, wo man für jeden Command eine Erklärung bekommen kann", inline=False)
-        #Ziel 2
-        embed.add_field(name=" kleine neu features für /wiki", value="- Suche mit Reaktion abbrechen \n- Logo con SoT Wiki in den Footer bei Ergebnissen", inline=False)
-        #Ziel 3
-        embed.add_field(name="/guide erweitern", value="Fort Blindspots zum Beispiel: https://seaofthieves.fandom.com/wiki/Fortress?file=FortVision.jpg", inline=False)
 
+        with open('./Glorious_Bilge_Bot/roadmap.txt', 'r') as file:
+            file_contents = file.read()      
+
+        embed=discord.Embed(title="Roadmap für den Glorious Bilge Bot", description=file_contents, color=0xc59d6d)
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
